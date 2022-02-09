be85badd32d8e5cd4550c6e8b73f408d0819424e
johanastrom <johan.astrom89@gmail.com>
2022-02-09 11:43:20 +0100

    Added SecurityConfig.java

--- src/main/java/se/astrom/complexjava/security/SecurityConfig.java
+++ src/main/java/se/astrom/complexjava/security/SecurityConfig.java
@@ -1,11 +1,49 @@
 package se.astrom.complexjava.security;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+import java.util.Date;
 
 @Configuration
 @EnableWebSecurity
-public class SecurityConfig {
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
+    private final M365LicensesUserDetailsService userDetailsService;
 
+    public SecurityConfig(M365LicensesUserDetailsService userDetailsService) {
+        this.userDetailsService = userDetailsService;
+    }
+
+    @Bean
+    public DaoAuthenticationProvider authenticationProvider(){
+        var provider = new DaoAuthenticationProvider();
+        provider.setUserDetailsService(userDetailsService);
+        provider.setPasswordEncoder(new BCryptPasswordEncoder());
+        return provider;
+    }
+
+    @Override
+    protected void configure(HttpSecurity httpSecurity) throws Exception{
+        httpSecurity
+                .httpBasic()
+                .and()
+                .authorizeRequests()
+                .antMatchers("/", "/home", "/users/signup").permitAll()
+                .antMatchers("/admin").hasRole("ADMIN")
+                .anyRequest().authenticated()
+                .and()
+                .formLogin()
+                .loginPage("/login").permitAll()
+                .and()
+                .logout()
+                .invalidateHttpSession(true)
+                .clearAuthentication(true)
+                .permitAll();
+    }
 }
