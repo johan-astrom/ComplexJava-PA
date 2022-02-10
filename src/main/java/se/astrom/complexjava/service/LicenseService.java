00e14edf1abb92d4a013f201aff088d5900b5df0
johanastrom <johan.astrom89@gmail.com>
2022-02-10 17:09:54 +0100

    Added banner

--- src/main/java/se/astrom/complexjava/service/LicenseService.java
+++ src/main/java/se/astrom/complexjava/service/LicenseService.java
@@ -1,5 +1,7 @@
 package se.astrom.complexjava.service;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 import se.astrom.complexjava.dto.Microsoft365LicenseDto;
 import se.astrom.complexjava.mapper.Microsoft365LicensesMapper;
@@ -10,6 +12,7 @@ public class LicenseService {
 
     private final Microsoft365LicenseRepository licenseRepository;
     private final Microsoft365LicensesMapper mapper;
+    private final Logger logger = LoggerFactory.getLogger(LicenseService.class);
 
     public LicenseService(Microsoft365LicenseRepository licenseRepository, Microsoft365LicensesMapper mapper) {
         this.licenseRepository = licenseRepository;
@@ -21,6 +24,7 @@ public class LicenseService {
     }
 
     public Microsoft365LicenseDto createLicense(Microsoft365LicenseDto licenseDto){
+        logger.info("licenseDto: " + licenseDto);
         return mapper.microsoft365LicenseToMicrosoft365LicenseDto(licenseRepository.save(mapper.microsoft365LicenseDtoToMicrosoft365License(licenseDto)));
     }
 }
