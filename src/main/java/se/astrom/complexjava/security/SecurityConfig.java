6f06cbb5f01c8ad2d9b1dda6933e8bcb80133430
johanastrom <johan.astrom89@gmail.com>
2022-02-11 17:20:43 +0100

    More jwt configs

--- src/main/java/se/astrom/complexjava/security/SecurityConfig.java
+++ src/main/java/se/astrom/complexjava/security/SecurityConfig.java
@@ -1,13 +1,20 @@
 package se.astrom.complexjava.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import se.astrom.complexjava.security.jwt.JwtAuthenticationEntryPoint;
+import se.astrom.complexjava.security.jwt.JwtRequestFilter;
 
 import java.util.Date;
 
@@ -16,13 +23,29 @@ import java.util.Date;
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     private final M365LicensesUserDetailsService userDetailsService;
-    private JwtAuthen
+    private final JwtAuthenticationEntryPoint jwtAuthenticationEntryPoint;
+    private final JwtRequestFilter jwtRequestFilter;
 
-    public SecurityConfig(M365LicensesUserDetailsService userDetailsService) {
+    public SecurityConfig(M365LicensesUserDetailsService userDetailsService,
+                          JwtAuthenticationEntryPoint jwtAuthenticationEntryPoint,
+                          JwtRequestFilter jwtRequestFilter) {
         this.userDetailsService = userDetailsService;
+        this.jwtAuthenticationEntryPoint = jwtAuthenticationEntryPoint;
+        this.jwtRequestFilter = jwtRequestFilter;
+    }
+
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception{
+        auth.userDetailsService(userDetailsService).passwordEncoder(passwordEncoder());
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
     }
 
     @Override
+    @Bean
     public AuthenticationManager authenticationManagerBean() throws Exception {
         return super.authenticationManagerBean();
     }
@@ -39,20 +62,27 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity httpSecurity) throws Exception{
         httpSecurity
                 .csrf().disable()
-                .httpBasic()
-                .and()
+//                .httpBasic()
+//                .and()
                 .authorizeRequests()
-                .antMatchers("/", "/home", "/users/signup", "/swaggerui", "/v2/api-docs").permitAll()
+                .antMatchers("/", "/authenticate", "/users/signup", "/swaggerui", "/v2/api-docs").permitAll()
                 .antMatchers("/users/createUserWithRole").hasRole("ADMIN")
                 .antMatchers("/azureUsers", "/licenses").hasAnyRole("ADMIN", "MANAGER")
                 .anyRequest().authenticated()
                 .and()
-                .formLogin()
-                .loginPage("/login").permitAll()
+                .exceptionHandling()
+                .authenticationEntryPoint(jwtAuthenticationEntryPoint)
                 .and()
-                .logout()
-                .invalidateHttpSession(true)
-                .clearAuthentication(true)
-                .permitAll();
+                .sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+//                .and()
+//                .formLogin()
+//                .loginPage("/login").permitAll()
+//                .and()
+//                .logout()
+//                .invalidateHttpSession(true)
+//                .clearAuthentication(true)
+//                .permitAll();
+        httpSecurity.addFilterBefore(jwtRequestFilter, UsernamePasswordAuthenticationFilter.class);
     }
 }
